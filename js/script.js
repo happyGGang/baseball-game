cb729634fd7c7673a72d8efe9cf936c34c2da6ba
joy.gang <joy.gang@dxworks.kr>
2022-09-20 16:16:07 +0900

    [fix] password 할당

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-;(function () {
+;(() => {
   'use strict'
 
   const get = (target) => document.querySelector(target)
 
   const init = () => {
-    get('form').addEventListener('submit', (e) => {
-      playGame(e)
+    get('form').addEventListener('submit', (event) => {
+      playGame(event)
     })
-
     setPassword()
   }
 
@@ -21,47 +20,43 @@
     $input: get('.ball_input'),
   }
 
-  // 구조분해 할당
-  const {limit, digit, $question, $answer, $input} = baseball
-  let {trial, end} = baseball
-  
-  /*  패스워드 지정 */
+  const { limit, digit, $question, $answer, $input } = baseball
+  let { trial, end } = baseball
+
   const setPassword = () => {
     const gameLimit = Array(limit).fill(false)
     let password = ''
-    while(password.length < digit) {
+    while (password.length < digit) {
       const random = parseInt(Math.random() * 10, 10)
 
-      if(gameLimit[random]) {
+      if (gameLimit[random]) {
         continue
       }
       password += random
       gameLimit[random] = true
     }
+
+    baseball.password = password
   }
 
-  /* 시도를 했을때 / nunber : 내가 입력한 숫자 / hint: 현재 어떤 상황인지? */
   const onPlayed = (number, hint) => {
-    return `<em>${trial}차 시도 : ${number}, ${hint}</em>`
+    return `<em>${trial}차 시도</em>: ${number}, ${hint}`
   }
 
-   /* 번호가 같은지 */
   const isCorrect = (number, answer) => {
     return number === answer
   }
 
-   /* 중복번호가 있는지 */
   const isDuplicate = (number) => {
     return [...new Set(number.split(''))].length !== digit
   }
 
- /* 스트라이크 갯수 */
   const getStrikes = (number, answer) => {
     let strike = 0
     const nums = number.split('')
 
     nums.map((digit, index) => {
-      if(digit === answer[index]) {
+      if (digit === answer[index]) {
         strike++
       }
     })
@@ -69,8 +64,6 @@
     return strike
   }
 
-
- /* 볼 카운트 갯수 */
   const getBalls = (number, answer) => {
     let ball = 0
     const nums = number.split('')
@@ -81,7 +74,7 @@
     })
 
     nums.map((num, index) => {
-      if(answer[index] !== num && !!gameLimit[num]) {
+      if (answer[index] !== num && !!gameLimit[num]) {
         ball++
       }
     })
@@ -89,50 +82,48 @@
     return ball
   }
 
- /* 시도에 따른 결과 */
   const getResult = (number, answer) => {
-    if(isCorrect(number, answer)) {
+    if (isCorrect(number, answer)) {
       end = true
       $answer.innerHTML = baseball.password
-      return '홈런!'
+      return '홈런!!'
     }
 
     const strikes = getStrikes(number, answer)
     const balls = getBalls(number, answer)
 
-    return 'STRIKE : ' + strikes + ' BALL : ' + balls
+    return 'STRIKE: ' + strikes + ', BALL: ' + balls
   }
 
-  /* 게임 플레이  */
-  const playGame = (e) => {
-    e.preventDefault();
-    
-    if(!!end) {
+  const playGame = (event) => {
+    event.preventDefault()
+
+    if (!!end) {
       return
     }
 
     const inputNumber = $input.value
     const { password } = baseball
 
-    if(inputNumber.length !== digit) {
+    if (inputNumber.length !== digit) {
       alert(`${digit}자리 숫자를 입력해주세요.`)
-    } else if(isDuplicate(inputNumber)) {
-      alert('중복되는 숫자가 있습니다.')
+    } else if (isDuplicate(inputNumber)) {
+      alert('중복 숫자가 있습니다.')
     } else {
       trial++
-
-      const result = onPlayed(inputNumber, getResult(inputNumber, password)) 
+      const result = onPlayed(inputNumber, getResult(inputNumber, password))
       $question.innerHTML += `<span>${result}</span>`
 
-      if(limit <= trial && !isCorrect(inputNumber, password)) {
+      if (limit <= trial && !isCorrect(inputNumber, password)) {
         alert('쓰리아웃!')
         end = true
+        $answer.innerHTML = password
       }
-      $answer.innerHTML = password
     }
+
     $input.value = ''
     $input.focus()
   }
 
-  init();
+  init()
 })()

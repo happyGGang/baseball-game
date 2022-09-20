a8898e9f09f1e3641454c42f024e4063c6ba64f6
joy.gang <joy.gang@dxworks.kr>
2022-09-20 11:32:36 +0900

    [update] playGame 함수 추가

--- js/script.js
+++ js/script.js
@@ -20,14 +20,19 @@
     $answer: get('.ball_answer'),
     $input: get('.ball_input'),
   }
+
+  // 구조분해 할당
+  const {limit, digit, $question, $answer, $input} = baseball
+  let {trial, end} = baseball
+
   /*  4자리 수를 모두 맞추었을 때 */
   const setHomeRun = () => {
 
   }
 
-  /* 시도를 했을때 */
-  const onPlayed = () => {
-
+  /* 시도를 했을때 / nunber : 내가 입력한 숫자 / hint: 현재 어떤 상황인지? */
+  const onPlayed = (number, hint) => {
+    return `<em>${trial}차 시도 : ${number}, ${hint}</em>`
   }
 
    /* 번호가 같은지 */
@@ -36,7 +41,7 @@
   }
 
    /* 중복번호가 있는지 */
-  const inDuplicate = () => {
+  const isDuplicate = () => {
 
   }
 
@@ -56,8 +61,34 @@
   }
 
   /* 게임 플레이  */
-  const playGame = () => {
+  const playGame = (e) => {
+    e.preventDefault();
+    
+    if(!!end) {
+      return
+    }
 
+    const inputNumber = $input.value
+    const { password } = baseball
+
+    if(inputNumber.length !== digit) {
+      alert(`${digit}자리 숫자를 입력해주세요.`)
+    } else if(isDuplicate(inputNumber)) {
+      alert('중복되는 숫자가 있습니다.')
+    } else {
+      trial++
+
+      const result = onPlayed(inputNumber, getResult(inputNumber, password)) 
+      $question.innerHTML += `<span>${result}</span>`
+
+      if(limit <= trial && !isCorrect(inputNumber, password)) {
+        alert('쓰리아웃!')
+        end = true
+      }
+      $answer.innerHTML = password
+    }
+    $input.value = ''
+    $input.focus()
   }
 
   init();
